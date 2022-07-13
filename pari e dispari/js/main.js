c94cbff64957fbbe070fd112667e428bfec23c00
Simone Fortunato <frtsmn94@gmail.com>
2022-07-13 08:42:43 +0200

    Continuazione pari e dispari

--- pari e dispari/js/main.js
+++ pari e dispari/js/main.js
@@ -1,27 +1,27 @@
 
-let numero_giocatore = 5;
-let scelta_giocatore = document.getElementById(PoD);
-/*
-console.log(numero_giocatore)
-let scelta_giocatore = document.getElementById(PoD);
-let gioca = document.getElementById(gioca);
-console.log(scelta_giocatore)
+let numero_giocatore = document.getElementById('numero').value;
+let scelta_giocatore = document.getElementById('PoD').value;
+let via = document.getElementById('gioca');
+let risultato;
+let numeroComputer;
+let somma;
 
-gioca.addEventListener("click", random(1,5)
 
-)
-*/
-let risultato
-let numeroComputer
-function random(min,max) {
-     numeroComputer = Math.floor(Math.random() * (max - min +1 )) + min;
-    console.log(numeroComputer)
+via.addEventListener("click", random(1,5), controllo());
+
+
+console.log(risultato)
+if( risultato == scelta_giocatore){
+    alert('Hai vinto');
+}else{
+    alert('ha vinto il computer');
 }
-random(1,5)
 
-let somma = (numeroComputer + numero_giocatore);
-console.log(somma)
+
+
+
 function controllo(){
+    somma = (numeroComputer + numero_giocatore);
     let divisione = somma % 2;
     if(divisione==0){
         risultato="Pari";
@@ -31,10 +31,7 @@ function controllo(){
     return risultato;
 }
 
-controllo();
-console.log(risultato)
-if( risultato == scelta_giocatore){
-    alert('Hai vinto');
-}else{
-    alert('ha vinto il computer');
+function random(min,max) {
+    numeroComputer = Math.floor(Math.random() * (max - min +1 )) + min;
+   console.log(numeroComputer)
 }
